806a483fb1b51b805fe71fcf89df052759ec785d
Rashmi Sharma <59957360+Rashmi1199@users.noreply.github.com>
2021-01-29 12:58:12 +0530

    Update 4-1 Binary Search.cpp
    // # PROBLEM STATEMENT: 
    
    // Task. The goal in this code problem is to implement the binary search algorithm.
    // Input Format. The first line of the input contains an integer 𝑛 and a sequence 𝑎0 < 𝑎1 < . . . < 𝑎𝑛−1 of 𝑛 pairwise distinct positive integers in increasing order. The next line contains an integer 𝑘 and 𝑘 positive integers 𝑏0, 𝑏1, . . . , 𝑏𝑘−1.
    // Constraints. 1 ≤ 𝑘 ≤ 105; 1 ≤ 𝑛 ≤ 3 · 104; 1 ≤ 𝑎𝑖 ≤ 109 for all 0 ≤ 𝑖 < 𝑛; 1 ≤ 𝑏𝑗 ≤ 109 for all 0 ≤ 𝑗 < 𝑘;
    // Output Format. For all 𝑖 from 0 to 𝑘 − 1, output an index 0 ≤ 𝑗 ≤ 𝑛 − 1 such that 𝑎𝑗 = 𝑏𝑖 or −1 if there is no such index.

--- Week 4/4-1 Binary Search.cpp
+++ Week 4/4-1 Binary Search.cpp
@@ -1,11 +1,4 @@
-# Good job! (Max time used: 0.05/1.00, max memory used: 40476672/536870912.)
-
-# PROBLEM STATEMENT: 
-
-Task. The goal in this code problem is to implement the binary search algorithm.
-Input Format. The first line of the input contains an integer 𝑛 and a sequence 𝑎0 < 𝑎1 < . . . < 𝑎𝑛−1 of 𝑛 pairwise distinct positive integers in increasing order. The next line contains an integer 𝑘 and 𝑘 positive integers 𝑏0, 𝑏1, . . . , 𝑏𝑘−1.
-Constraints. 1 ≤ 𝑘 ≤ 105; 1 ≤ 𝑛 ≤ 3 · 104; 1 ≤ 𝑎𝑖 ≤ 109 for all 0 ≤ 𝑖 < 𝑛; 1 ≤ 𝑏𝑗 ≤ 109 for all 0 ≤ 𝑗 < 𝑘;
-Output Format. For all 𝑖 from 0 to 𝑘 − 1, output an index 0 ≤ 𝑗 ≤ 𝑛 − 1 such that 𝑎𝑗 = 𝑏𝑖 or −1 if there is no such index.
+// # Good job! (Max time used: 0.05/1.00, max memory used: 40476672/536870912.)
 
 #include <iostream>
 #include <cassert>
